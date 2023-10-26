f67926f7a9715841316c8e7d3cb14aab7826c142
Bottesz <botyaring@gmail.com>
2023-10-26 21:25:24 +0200

    hazipotlas feladatok

---
+++ main.py
@@ -0,0 +1,4 @@
+import ciklusok
+ciklusok.harommal_oszthato_szamok()
+ciklusok.csokkeno_sorrend()
+ciklusok.szamok_kiirasa()
---
+++ tesztesetek.py
@@ -0,0 +1,10 @@
+ciklusok.harommal_oszthato_szamok()
+szam = int(input("Kérem a számot: "))
+
+
+ciklusok.csokkeno_sorrend()
+szam = int(input("Kérem a számot: "))
+
+
+ciklusok.szamok_kiirasa()
+szam = int(input("Kérem a számot: "))
